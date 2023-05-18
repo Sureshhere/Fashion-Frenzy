af024631b94bdb1a002e17a1916d8f9c5ed78110
Suresh <2111cs030133@mallareddyuniversity.ac.in>
2023-05-18 18:57:31 +0530

    shop page added

---
+++ productsArray.js
@@ -0,0 +1,67 @@
+var products = [
+    { 
+        name: 'HEART MAXY GOWN - WHITE MOTI SUIT',
+        price: 929, rating: '⭐️⭐️⭐⭐', 
+        image: '/img/products/f4.jpg', 
+        details: 'Sizes available :- S, M, L, XL and XXL', 
+        keywords: ['women', 'suit', 'ladies', 'gown'],
+        id:'f4.html'
+    },
+    { 
+        name: 'TRADITIONAL CHIKANKARI DELIGHT',
+        price: 979, 
+        rating: '⭐️⭐️⭐⭐', 
+        image: '/img/products/f8.jpg', 
+        details: 'Sizes available :- M, L, XL and XXL', 
+        keywords: ['women', 'suit', 'ladies', 'top','plazo','chikankari','traditional'],
+        id:'f8.html'
+    },   
+    { 
+        name: 'MIDNIGHT GARDEN GOWN',
+        price: 1149, rating: '⭐️⭐️⭐⭐⭐', 
+        image: '/img/products/f1.jpg', 
+        details: 'Sizes available :- S, M, L, XL and XXL', 
+        keywords: ['women', 'suit', 'ladies', 'gown','Sleeveless','flower'],
+        id:'f1.html'
+    },
+    { 
+        name: 'TOMMY CASUAL STRIPED T-SHIRT',
+        price: 699, rating: '⭐️⭐️⭐⭐⭐', 
+        image: 'https://thefashionfrenzy.vercel.app/featured-products/fp-images/f2.jpg', 
+        details: 'Sizes available :- M, L, XL', 
+        keywords: ['men', 'tshirt', 't-shirt', 't shirt','mens','casual','tommy','casual t-shirt','cotton'],
+        id:'f2.html'
+    },
+    { 
+        name: 'DENIM FABRIC SHIRT',
+        price: 649, rating: '⭐️⭐️⭐⭐⭐', 
+        image: '/img/products/f6.jpg', 
+        details: 'Sizes available :-  M, L, XL and XXL', 
+        keywords: ['men', 'shirt', 'denim', 'gucci','cotton','casual','casual t-shirt'],
+        id:'f6.html'
+    },
+    { 
+        name: 'LEVI POPCORN MATTY T-SHIRT',
+        price: 520, rating: '⭐️⭐️⭐⭐', 
+        image: '/img/products/f3.jpg', 
+        details: 'Sizes available :- M, L, XL and XXL', 
+        keywords: ['men', 'shirt', 'full sleeves', 'levi','cotton','casual',],
+        id:'f3.html'
+    },
+    { 
+        name: 'TOMMY HILFIGER',
+        price: 699, rating: '⭐️⭐️⭐', 
+        image: '/img/products/f5.jpg', 
+        details: 'Sizes available :- M, L, XL', 
+        keywords: ['men', 'shirt', 'denim','cotton','casual','tommy','casual t-shirt'],
+        id:'f5.html'
+    },
+    { 
+        name: 'KOTI TOP LEHENGA',
+        price: 1499, rating: '⭐️⭐️⭐⭐', 
+        image: 'https://thefashionfrenzy.vercel.app/featured-products/fp-images/f7_3.jpg', 
+        details: 'Sizes available :- XL, XXL', 
+        keywords: ['women', 'lehenga', 'party wear','Georgette','cotton','koti top'],
+        id:'f5.html'
+    }
+];
